e9eb7cc66a054224977b6fa1fa6d990bbd862a93
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-05-28 20:24:08 +0300

    sayısal loto uygulaması tamamlandı

--- SayisalLotoUygulamasi/Form1.Designer.cs
+++ SayisalLotoUygulamasi/Form1.Designer.cs
@@ -29,13 +29,134 @@ namespace SayisalLotoUygulamasi
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 129);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Top1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(315, 129);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Top2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(496, 129);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Top3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(708, 129);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 37);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Top4";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 195);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 53);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(278, 195);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(166, 53);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(478, 195);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(166, 53);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(672, 195);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(166, 53);
+            this.textBox4.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(323, 293);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(325, 111);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Çekilişi Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1511, 832);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("MV Boli", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- SayisalLotoUygulamasi/Form1.cs
+++ SayisalLotoUygulamasi/Form1.cs
@@ -16,5 +16,60 @@ namespace SayisalLotoUygulamasi
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rast = new Random();
+            int s1, s2, s3, s4;
+            s1 = rast.Next(1, 5);
+            s2 = rast.Next(1, 5);
+            s3 = rast.Next(1, 5);
+            s4 = rast.Next(1, 5);
+            label1.Text = s1.ToString();
+            label2.Text = s2.ToString();
+            label3.Text = s3.ToString();
+            label4.Text = s4.ToString();
+
+            //sayı 1
+            if (textBox1.Text == label1.Text)
+            {
+                textBox1.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox1.BackColor = Color.Red;
+            }
+
+            //sayı 2
+            if (textBox2.Text == label2.Text)
+            {
+                textBox2.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox2.BackColor = Color.Red;
+            }
+
+            //sayı 3
+            if (textBox3.Text == label3.Text)
+            {
+                textBox3.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox3.BackColor = Color.Red;
+            }
+
+            //sayı 4
+            if (textBox4.Text == label4.Text)
+            {
+                textBox4.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox4.BackColor = Color.Red;
+            }           
+
+        }
     }
 }
